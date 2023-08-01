b12fc3bea32d4f82346584aaa9def441d3e3005f
Esteban-Reyes-Anguiano <139920025+Esteban-Reyes-Anguiano@users.noreply.github.com>
2023-07-31 22:22:38 -0600

    lab completado Auth servis

--- __tests__/AuthService.test.ts
+++ __tests__/AuthService.test.ts
@@ -1,3 +1,33 @@
-test('Hello world test', () => {
-  expect('Hello world').toBe('Hello world!');
+import AuthService from '../src/AuthService';
+
+
+describe('AuthService', () => {
+  let authService: AuthService;
+
+  beforeEach(() => {
+    authService = new AuthService();
+  });
+
+  it('debería registrar un nuevo usuario exitosamente', async () => {
+    const resultado = await authService.register('usuario123', 'contraseña123');
+    expect(resultado).toBe(true);
+  });
+
+  it('no debería registrar un usuario con el mismo nombre de usuario', async () => {
+    await authService.register('usuario456', 'contraseña456');
+    const resultado = await authService.register('usuario456', 'contraseña789');
+    expect(resultado).toBe(false);
+  });
+
+  it('debería iniciar sesión con credenciales válidas', async () => {
+    await authService.register('usuario789', 'contraseña789');
+    const resultado = await authService.login('usuario789', 'contraseña789');
+    expect(resultado).toBe(true);
+  });
+
+  it('no debería iniciar sesión con credenciales inválidas', async () => {
+    await authService.register('usuario101', 'contraseña101');
+    const resultado = await authService.login('usuario101', 'contraseña_incorrecta');
+    expect(resultado).toBe(false);
+  });
 });
